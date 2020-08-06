84e4a1218cc099580dbbe2aeffe0fa52fe609e2d
Neil Lyons <nwjlyons@googlemail.com>
2020-08-06 12:44:55 +0100

    Reorder functions to match exection order

--- lib/mix/tasks/phx.gen.auth.passwordless.ex
+++ lib/mix/tasks/phx.gen.auth.passwordless.ex
@@ -18,6 +18,13 @@ defmodule Mix.Tasks.Phx.Gen.Auth.Passwordless do
     |> copy_new_files(binding, paths)
   end
 
+  defp copy_new_files(%Context{} = context, binding, paths) do
+    files = files_to_be_generated(context)
+    Mix.Phoenix.copy_from(paths, "priv/templates/phx.gen.auth.passwordless", binding, files)
+
+    context
+  end
+
   defp files_to_be_generated(%Context{schema: schema, context_app: context_app} = context) do
     web_prefix = Mix.Phoenix.web_path(context_app)
     web_test_prefix = Mix.Phoenix.web_test_path(context_app)
@@ -29,12 +36,5 @@ defmodule Mix.Tasks.Phx.Gen.Auth.Passwordless do
       {:eex, "schema_user_sign_in_code.ex", Path.join([context.dir, "#{schema.singular}_sign_in_code.ex"])},
       {:eex, "context.ex", "#{context.dir}.ex"}
     ]
-  end
-
-  defp copy_new_files(%Context{} = context, binding, paths) do
-    files = files_to_be_generated(context)
-    Mix.Phoenix.copy_from(paths, "priv/templates/phx.gen.auth.passwordless", binding, files)
-
-    context
   end
 end
