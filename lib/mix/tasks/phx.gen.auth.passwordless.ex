13a3b2f92740cbd7781457d10b3dfcccc773fc20
Neil Lyons <nwjlyons@googlemail.com>
2020-08-06 10:34:04 +0100

    Add initial mix task generator

---
+++ lib/mix/tasks/phx.gen.auth.passwordless.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.Phx.Gen.Auth.Passwordless do
+  @shortdoc "Generates passwordless authentication logic for a resource"
+
+  use Mix.Task
+
+  def run(args) do
+    IO.puts("Hello from Mix.Tasks.Phx.Gen.Auth.Passwordless")
+  end
+end
