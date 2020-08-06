c8ae2829f7d8e0d20a547db16f0392b9a59c5f77
Neil Lyons <nwjlyons@googlemail.com>
2020-08-06 10:14:35 +0100

    Initial commit

---
+++ lib/phx_gen_auth_passwordless.ex
@@ -0,0 +1,18 @@
+defmodule PhxGenAuthPasswordless do
+  @moduledoc """
+  Documentation for `PhxGenAuthPasswordless`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxGenAuthPasswordless.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_gen_auth_passwordless_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxGenAuthPasswordlessTest do
+  use ExUnit.Case
+  doctest PhxGenAuthPasswordless
+
+  test "greets the world" do
+    assert PhxGenAuthPasswordless.hello() == :world
+  end
+end
