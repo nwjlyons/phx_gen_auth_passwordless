bacfb6af873017b0c61fc3d30e5de21cc3bae890
Neil Lyons <nwjlyons@googlemail.com>
2020-08-07 12:13:42 +0100

    Add dynamic table name to migration

--- priv/templates/phx.gen.auth.passwordless/migration.ex
+++ priv/templates/phx.gen.auth.passwordless/migration.ex
@@ -4,15 +4,15 @@ defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.
   def change do
     execute "CREATE EXTENSION IF NOT EXISTS citext", ""
 
-    create table(:users) do
+    create table(:<%= schema.table %>) do
       add :email, :citext
 
       timestamps()
     end
 
-    create unique_index(:users, [:email])
+    create unique_index(:<%= schema.table %>, [:email])
 
-    create table(:users_sign_in_codes) do
+    create table(:<%= schema.table %>_sign_in_codes) do
       add :hashed_code, :string, null: false
       add :user_id, references(:users, on_delete: :delete_all), null: false
       add :sign_in_attempts, :integer, null: false, default: 0
